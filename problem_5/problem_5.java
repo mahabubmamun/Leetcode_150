0dc1d6d7ebe6801393a975f373507403161ae5e8
Mahabub Hasan Mamun <102401671+mahabubmamun@users.noreply.github.com>
2025-09-20 00:21:50 +0600

    problem_5.java

---
+++ problem_5/problem_5.java
@@ -0,0 +1,59 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        Arrays.sort(nums);
+        int temp = 1;
+        int res = nums[0];
+        int size = nums.length;
+        for(int i=0;i<size-1;i++)
+        {
+            if(nums[i] == nums[i+1])
+            {
+                temp++;
+            }
+            else
+            {
+                if(temp > size/2)
+                {
+                    return nums[i];
+                    // break;
+                }
+                temp = 1;
+            }
+        }
+        if(temp > size/2)
+        {
+            return nums[size-1];
+        }
+        return res;
+
+
+        // solution 2
+        // using sorting
+        // Arrays.sort(nums);
+        // int size = nums.length;
+        // int res = nums[size/2];
+        // return res;
+
+
+        // solution 3
+        // using Moore Voting Algorithm
+        // int count = 0;
+        // int candidate = 0;
+        // for(int num : nums)
+        // {
+        //     if(count == 0)
+        //     {
+        //         candidate = num;
+        //     }
+        //     if(num == candidate)
+        //     {
+        //         count++;
+        //     }
+        //     else
+        //     {
+        //         count--;
+        //     }
+        // }
+        // return candidate;
+    }
+}
